1ab0d36cb473ae78d0d37f362cf6487768692db1
dfjhlsdhf <13728808660@163.com>
2022-12-06 13:52:07 -0500

    create Currency.java

---
+++ bankATM/src/Currency.java
@@ -0,0 +1,31 @@
+/**
+ * @author cqin
+ * @project_name bank_final_project
+ * @date 2022/12/6
+ */
+
+public class Currency {
+    private double value;  //the value of currency
+    private String kind; //kind of currency  US dollar, pound, RMB, Rupee
+
+    public Currency(double value, String kind) {
+        this.value = value;
+        this.kind = kind;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+}
