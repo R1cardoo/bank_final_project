c00f5c08f7bba005839bbdf6194aa5c52902b915
Rhythm Somaiya <Rhythmsomaiya31@gmail.com>
2022-12-11 23:25:12 -0500

    Update Constants.java

--- bankATM/src/Constants.java
+++ bankATM/src/Constants.java
@@ -4,7 +4,14 @@
 public class Constants {
 
     private static double minimumSavingsAccountBalanceForSecurities = 5000.00;
-    private static double savingsAccountInterestPercentage = 0.02;
+    private static double savingsAccountInterestPercentage = 0.03;
+    private static double openAccountFee = 15.00;
+    private static double closeAccountFee = 25.00;
+    private static double minOpenAccountBalance = 100.00;
+    private static double minOpenSecuritiesAccountBalance = 1000.00;
+    private static double loanInterestPercentage = 0.1;
+    private static double withdrawFeePercentage = 0.01;
+    private static double transactionFeeRate = 0.05;
 
     public static double getMinimumSavingsAccountBalanceForSecurities() {
         return minimumSavingsAccountBalanceForSecurities;
@@ -12,6 +19,34 @@ public class Constants {
 
     public static double getSavingsAccountInterestPercentage() {
         return savingsAccountInterestPercentage;
+    }
+
+    public static double getOpenAccountFee() {
+        return openAccountFee;
+    }
+
+    public static double getCloseAccountFee() {
+        return closeAccountFee;
+    }
+
+    public static double getMinOpenAccountBalance() {
+        return minOpenAccountBalance;
+    }
+
+    public static double getMinOpenSecuritiesAccountBalance() {
+        return minOpenSecuritiesAccountBalance;
+    }
+
+    public static double getLoanInterestPercentage() {
+        return loanInterestPercentage;
+    }
+
+    public static double getWithdrawFeePercentage() {
+        return withdrawFeePercentage;
+    }
+
+    public static double getTransactionFeeRate() {
+        return transactionFeeRate;
     }
 
 }
