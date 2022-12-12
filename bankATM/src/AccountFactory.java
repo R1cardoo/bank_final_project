b2171ca965d4752035b26d93f9197dbb7a13d2d6
Rhythm Somaiya <Rhythmsomaiya31@gmail.com>
2022-12-11 23:26:44 -0500

    Please confirm: AccountHelper & AccountFactory

---
+++ bankATM/src/AccountFactory.java
@@ -0,0 +1,12 @@
+public class AccountFactory { //confirm this because I'm not sure about this
+
+    public static Account createAccount() {
+        Account account = new Account();
+        account.setUsername(account.getUsername());
+        account.setType(account.getType());
+        account.setBalance(account.getBalance());
+        account.setCurrencies(account.getCurrencies());
+        return account;
+    }
+
+}
---
+++ bankATM/src/AccountHelper.java
@@ -0,0 +1,55 @@
+public class AccountHelper { //confirm this because I'm not sure about how this would work
+
+    private static AccountFactory factory;
+    private static Account account;
+
+    public Account createAccount() {
+        return factory.createAccount();
+    }
+
+    public boolean saveAccountInfo() {
+        return true;
+    }
+
+    public boolean applyLoans() {
+        return true;
+    }
+
+    public boolean transfer() {
+        return true;
+    }
+
+    public boolean withdrawl() {
+        return true;
+    }
+
+    public boolean requestLoan() {
+        return true;
+    }
+
+    public boolean updateAccount() {
+        return true;
+    }
+
+    public boolean balanceAdd() {
+        return true;
+    }
+
+    public boolean balanceMinus() {
+        return true;
+    }
+
+    public boolean buyStock() {
+        if (isRich()) {
+            account.buyStock();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean sellStock() {
+        return true;
+    }
+
+}
